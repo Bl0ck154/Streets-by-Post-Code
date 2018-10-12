a1822012dd60e08a9af69c19fdb94f3764dca1e3
Bl0ck <zail@dublin.com>
2018-10-12 21:38:59 +0300

    https://github.com/Bl0ck154/Streets-by-Post-Code.git

--- ClientServerZipCodeStreetList/Program.cs
+++ ClientServerZipCodeStreetList/Program.cs
@@ -1,15 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
-namespace ClientServerZipCodeStreetList
+namespace ServerZipCodeStreetList
 {
+	/*Используя класс Socket реализовать клиент-серверное приложение,
+	которое позволяет клиенту по почтовому индексу получить список улиц, 
+	соответствующих этому индексу. Данные об улицах могут хранится в файловой или реляционной базе данных или в xml файле. 
+	клиент - Windows Forms или WPF приложение, 
+	сервер - консольное приложение. Запрос клиента выполняется в отдельном (не интерфейсном) потоке, 
+	сервер - синхронный.*/
 	class Program
 	{
+		static int port = 8005; // server port
 		static void Main(string[] args)
 		{
+			IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, port);
+			Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			try
+			{
+				listenSocket.Bind(iPEndPoint);
+
+				listenSocket.Listen(100);
+				Console.WriteLine("Server started. Waiting for messages...");
+				while (true)
+				{
+					Socket handle = listenSocket.Accept();
+					if (handle.Available > 0)
+						Task.Run(() => taskRun(handle));
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message, "Error");
+				Console.ReadKey();
+			}
+		}
+		static void taskRun(Socket handle)
+		{
+			Console.WriteLine($"{handle.RemoteEndPoint}");
+
+			string query = "";
+			byte[] buffer = new byte[1024];
+			int bytes = 0;
+			while (handle?.Available > 0)
+			{
+				bytes = handle.Receive(buffer);
+				query += Encoding.Unicode.GetString(buffer);
+			}
+
+			query = query.Replace("\0", string.Empty);
+			Console.WriteLine(DateTime.Now.ToString() + ": " + query);
+
+			List<string> answer = FindInXml(query);
+			handle?.Send(ObjectToBytesArray(answer));
+
+			handle?.Shutdown(SocketShutdown.Both);
+			handle?.Close();
+		}
+		static List<string> FindInXml(string str)
+		{
+			List<string> list = new List<string>(); ;
+
+			XmlDocument xmlDocument = new XmlDocument();
+			xmlDocument.Load("data.xml");
+			XmlNode finded = xmlDocument.SelectSingleNode($"//ZipCode[@Code='{str}']");
+			if (finded != null)
+			{
+				foreach (XmlNode item in finded.ChildNodes)
+				{
+					list.Add(item.InnerText);
+				}
+			}
+
+			return list;
+		}
+		static byte[] ObjectToBytesArray(object obj)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			MemoryStream memoryStream = new MemoryStream();
+			binaryFormatter.Serialize(memoryStream, obj);
+			return memoryStream.ToArray();
 		}
 	}
 }
---
+++ ClientZipCodeStreetList/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientZipCodeStreetList
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		int ServerPort = 8005; // server port
+		string ServerIP = "127.0.0.1";
+		public MainWindow()
+		{
+			InitializeComponent();
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+		}
+
+		private void FindButton_Click(object sender, RoutedEventArgs e)
+		{
+			IPAddress ip = IPAddress.Parse(ServerIP);
+			IPEndPoint ep = new IPEndPoint(ip, ServerPort);
+			Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			try
+			{
+				s.Connect(ep);
+				if (s.Connected)
+				{
+					string message = textboxQuery.Text.Trim();
+					byte[] data = Encoding.Unicode.GetBytes(message);
+
+					s.Send(data);
+					int bytes = 0;
+					List<byte> answer = new List<byte>();
+					do
+					{
+						bytes = s.Receive(data);
+						answer.AddRange(data);
+					} while (bytes > 0);
+
+					listBox.ItemsSource = FromBytesArrayToObject(answer.ToArray()) as List<string>;
+				}
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				try
+				{
+					s?.Shutdown(SocketShutdown.Both);
+					s?.Close();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			}
+		}
+		private object FromBytesArrayToObject(byte[] arr)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			MemoryStream memoryStream = new MemoryStream();
+			memoryStream.Write(arr, 0, arr.Length);
+			memoryStream.Position = 0;
+			return binaryFormatter.Deserialize(memoryStream);
+		}
+
+		private void textboxQuery_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			if (!Char.IsDigit(e.Text.FirstOrDefault()))
+				e.Handled = true;
+		}
+
+		private void TextBlock_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+				FindButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+		}
+	}
+}
